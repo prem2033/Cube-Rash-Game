59fc4d2601282e605c86eccc4a2db981ca7eda1d
prem prakash <premprakash2033@gmail.com>
2020-09-26 01:59:20 +0530

    animation and next level ontroduced

---
+++ Assets/Script/OnLevelCompleteNextScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnLevelCompleteNextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void NextLevel(){
+        Debug.Log("next level");
+        Invoke("loadNextLevel",4);
+    }
+    public void loadNextLevel(){
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
